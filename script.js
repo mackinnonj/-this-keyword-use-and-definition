59ef0adf5e29fe3b7cb20804fd42062ba88ae2c9
mackinnonj <102379816+mackinnonj@users.noreply.github.com>
2022-10-16 10:15:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,21 @@ obj1.importantPerson() //returns Cassy!
 obj2.importantPerson() //returns Jacob!
 
 // Code explanation: obj1.importantPerson() calls the improtant person peramitter in the obj1 object which calls the importantPerson function which uses "this" which is obj1.name which is equal to Cassy
+
+//Another example:
+const a = function() {
+    console.log('a', this)
+    const b = function() {
+        console.log('b', this)
+        const c = {
+            hi: function() {
+                console.log('c', this)
+            }
+        }
+        c.hi()
+    }
+    b()
+}
+a()
+
+// Window calls a() and Window also calls b(). Yes, the b function is in the a function, but the window called "b". There is nothing to the left of b() where when we call "c.hi()" the "this" object is the property "hi" of the function "c"
